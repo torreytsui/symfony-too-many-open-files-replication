35a32f25cb079cd120fe83b4689954c4dd186341
Torrey Tsui <torreytsui@gmail.com>
2019-07-18 19:59:00 +0100

    Initialise project

---
+++ config/bundles.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) To Tsui <torreytsui@gmail.com>
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+];
